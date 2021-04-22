fce22bb484e84a2c4a704a2ad3f35d784f3362e5
Abdul Samad <abdulsamamd9004607@gmail.com>
2021-04-22 18:25:01 +0500

    update5

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ var sehri = ['4:51am', '4:50am', '4:49am', ' 4:48am',' 4:47am','4:46am','4:45am'
   ' 4:34am','4:33am',' 4:32am','4:31am','4:30am',' 4:29am','4:28am',' 4:28am',
 ' 4:27am','4:26am','4:25am','4:24am','4:24am'];
 
-var iftari =[
-    '6:55pm','6:55pm','6:56pm','6:56pm','6:57pm','6:57pm','6:57pm','6:58pm','6:58pm','6:59pm',
+var iftari =['6:55pm','6:55pm','6:56pm','6:56pm','6:57pm','6:57pm','6:57pm','6:58pm','6:58pm','6:59pm',
 '6:59pm','7:00pm','7:00pm','7:01pm','7:01pm','7:02pm','7:02pm','7:03pm','7:03pm','7:04pm',
 '7:04pm','7:05pm','7:05pm','7:06pm','7:06pm','7:07pm','7:07pm','7:08pm','7:08pm',
 '7:09pm'];
@@ -13,97 +12,11 @@ var a = prompt('Do you want to know Sehr and Iftar timing of Ramadan 2021? Searc
 
 
 
-if(a == 1){
-  alert(a + ' Ramadan' + '\n' +'Sehri : ' + sehri[0] + '\n' + 'Iftari : ' + iftari[0])
+if(a > 0 && a <= 30){
+  alert(a + ' Ramadan' + '\n' +'Sehri : ' + sehri[a-1] + '\n' + 'Iftari : ' + iftari[a-1]);
  
 }
-else if(a == 2){
-  alert(a + ' Ramadan' + '\n' +'Sehri : ' + sehri[1] + '\n' + 'Iftari : ' + iftari[1])
-}
-else if(a == 3){
-  alert(a + ' Ramadan' + '\n' +'Sehri : ' + sehri[2] + '\n' + 'Iftari : ' + iftari[2])
-}
-else if(a == 4){
-  alert(a + ' Ramadan' + '\n' +'Sehri : ' + sehri[3] + '\n' + 'Iftari : ' + iftari[3])
-}
-else if(a == 5){
-  alert(a + ' Ramadan' + '\n' +'Sehri : ' + sehri[4] + '\n' + 'Iftari : ' + iftari[4])
-}
-else if(a == 6){
-  alert(a + ' Ramadan' + '\n' +'Sehri : ' + sehri[5] + '\n' + 'Iftari : ' + iftari[5])
-}
-else if(a == 7){
-  alert(a + ' Ramadan' + '\n' +'Sehri : ' + sehri[6] + '\n' + 'Iftari : ' + iftari[6])
-}
-else if(a == 8){
-  alert(a + ' Ramadan' + '\n' +'Sehri : ' + sehri[7] + '\n' + 'Iftari : ' + iftari[7])
-}
-else if(a == 9){
-  alert(a + ' Ramadan' + '\n' +'Sehri : ' + sehri[8] + '\n' + 'Iftari : ' + iftari[8])
-}
-else if(a == 10){
-  alert(a + ' Ramadan' + '\n' +'Sehri : ' + sehri[9] + '\n' + 'Iftari : ' + iftari[9])
-}
-else if(a == 11){
-  alert(a + ' Ramadan' + '\n' +'Sehri : ' + sehri[10] + '\n' + 'Iftari : ' + iftari[10])
-}
-else if(a == 12){
-  alert(a + ' Ramadan' + '\n' +'Sehri : ' + sehri[11] + '\n' + 'Iftari : ' + iftari[11])
-}
-else if(a == 13){
-  alert(a + ' Ramadan' + '\n' +'Sehri : ' + sehri[12] + '\n' + 'Iftari : ' + iftari[12])
-}
-else if(a == 14){
-  alert(a + ' Ramadan' + '\n' +'Sehri : ' + sehri[13] + '\n' + 'Iftari : ' + iftari[13])
-}
-else if(a == 15){
-  alert(a + ' Ramadan' + '\n' +'Sehri : ' + sehri[14] + '\n' + 'Iftari : ' + iftari[14])
-}
-else if(a == 16){
-  alert(a + ' Ramadan' + '\n' +'Sehri : ' + sehri[15] + '\n' + 'Iftari : ' + iftari[15])
-}
-else if(a == 17){
-  alert(a + ' Ramadan' + '\n' +'Sehri : ' + sehri[16] + '\n' + 'Iftari : ' + iftari[16])
-}
-else if(a == 18){
-  alert(a + ' Ramadan' + '\n' +'Sehri : ' + sehri[17] + '\n' + 'Iftari : ' + iftari[17])
-}
-else if(a == 19){
-  alert(a + ' Ramadan' + '\n' +'Sehri : ' + sehri[18] + '\n' + 'Iftari : ' + iftari[18])
-}
-else if(a == 20){
-  alert(a + ' Ramadan' + '\n' +'Sehri : ' + sehri[19] + '\n' + 'Iftari : ' + iftari[19])
-}
-else if(a == 21){
-  alert(a + ' Ramadan' + '\n' +'Sehri : ' + sehri[20] + '\n' + 'Iftari : ' + iftari[20])
-}
-else if(a == 22){
-  alert(a + ' Ramadan' + '\n' +'Sehri : ' + sehri[21] + '\n' + 'Iftari : ' + iftari[21])
-}
-else if(a == 23){
-  alert(a + ' Ramadan' + '\n' +'Sehri : ' + sehri[22] + '\n' + 'Iftari : ' + iftari[22])
-}
-else if(a == 24){
-  alert(a + ' Ramadan' + '\n' +'Sehri : ' + sehri[23] + '\n' + 'Iftari : ' + iftari[23])
-}
-else if(a == 25){
-  alert(a + ' Ramadan' + '\n' +'Sehri : ' + sehri[24] + '\n' + 'Iftari : ' + iftari[24])
-}
-else if(a == 26){
-  alert(a + ' Ramadan' + '\n' +'Sehri : ' + sehri[25] + '\n' + 'Iftari : ' + iftari[25])
-}
-else if(a == 27){
-  alert(a + ' Ramadan' + '\n' +'Sehri : ' + sehri[26] + '\n' + 'Iftari : ' + iftari[26])
-}
-else if(a == 28){
-  alert(a + ' Ramadan' + '\n' +'Sehri : ' + sehri[27] + '\n' + 'Iftari : ' + iftari[27])
-}
-else if(a == 29){
-  alert(a + ' Ramadan' + '\n' +'Sehri : ' + sehri[28] + '\n' + 'Iftari : ' + iftari[28])
-}
-else if(a == 30){
-  alert(a + ' Ramadan' + '\n' +'Sehri : ' + sehri[29] + '\n' + 'Iftari : ' + iftari[29])
-}
+
 else{
   alert('Please enter correct date' + '\n' + 'from 1-30')
   
